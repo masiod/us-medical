e5e5911453b928a0ddbb462b61487844feb436c7
Abdulrahman ALkathami <abda.a.e@hotmail.com>
2023-11-01 22:26:36 +0300

    change in us-medical.py uncomment some lines for the code

--- us-medical.py
+++ us-medical.py
@@ -26,32 +26,32 @@ with open('insurance.csv') as insurance_file:
         }})
 
 
-# def avg_of_patient(list):
-#     return round(sum(list)/len(list))
+def avg_of_patient(list):
+    return round(sum(list)/len(list))
 
 
-# male_cost_avg = []
-# female_cost_avg = []
+male_cost_avg = []
+female_cost_avg = []
 
 
-# def cost_avg_gender(dict):
-#     count = 0
-#     for key, values in dict.items():
-#         for k, v in values.items():
-#             count += 1
-#             if v == 'male':
-#                 male_cost_avg.append(dict[key]['charges'])
-#             elif v == 'female':
-#                 female_cost_avg.append(dict[key]['charges'])
-#     male_avg = float(sum(male_cost_avg)/len(male_cost_avg))
-#     female_avg = float(sum(female_cost_avg)/len(female_cost_avg))
-#     print(f'avg fro males in insurance is {male_avg}')
-#     print(f'avg fro females in insurance is {female_avg}')
+def cost_avg_gender(dict):
+    count = 0
+    for key, values in dict.items():
+        for k, v in values.items():
+            count += 1
+            if v == 'male':
+                male_cost_avg.append(dict[key]['charges'])
+            elif v == 'female':
+                female_cost_avg.append(dict[key]['charges'])
+    male_avg = float(sum(male_cost_avg)/len(male_cost_avg))
+    female_avg = float(sum(female_cost_avg)/len(female_cost_avg))
+    print(f'avg fro males in insurance is {male_avg}')
+    print(f'avg fro females in insurance is {female_avg}')
 
-#     return male_cost_avg, female_cost_avg
+    return male_cost_avg, female_cost_avg
 
 
-# cost_avg_gender(insurance_dict_all)
+cost_avg_gender(insurance_dict_all)
 
 
 dict_for_all_by_region = {
